485b63e07da7f01b8013ac92f4846f117738900e
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-02-28 18:32:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{
+  public static void main(String[]args){
+    //To Identify full sentences, or type a line, or names etc..., we need to chan ge our variable from "char" to "String"
+    //String name ="What's up, I am Joe.";
+    String name="What's up. I am joe.";
+    System.out.println(name);
+  }
+}
